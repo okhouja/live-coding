4941905a5becbe5cd9117021c8e19eb30f658083
Omar <omar0940@gmail.com>
2021-01-11 14:41:44 +0100

    functions Arrey

--- jan/11-01/index.js
+++ jan/11-01/index.js
@@ -197,13 +197,25 @@ const print = (str) => {
     text += i + " ";
   }
   console.log(text);
-  // 1 1 1 2 2 2 3 3 3 4 4 4
-  let countNum = 0;
-for (let i = 0; i <= 4; i++) {
-  console.log(i);
-  if (countNum != 0 && countNum % 3 == 0) {
-    i = 0;
+ 
+// 1 1 1 1 2 2 2 2 3 3 3 3 4 4 4 4
+let countNum = 0;
+text = "";
+for (let i = 1; i <= 4; ) {
+  text += i + " ";
+  if (countNum !== 0 && countNum % 3 === 0) {
+    i++;
     countNum = 0;
   }
   countNum++;
 }
+console.log(text);
+
+let st = "Hi this is me 12343 334";
+let stToArr = st.split(" ");
+console.log(stToArr);
+let backToSt = stToArr.join(" ");
+console.log(backToSt);
+
+console.log("1".repeat(3));
+console.log("2".repeat(3));
