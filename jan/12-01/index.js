010892c65ae8629a1e99fb41fb9ed11468f50493
Omar <omar0940@gmail.com>
2021-01-12 15:43:29 +0100

    Arrey Assignments

--- jan/12-01/index.js
+++ jan/12-01/index.js
@@ -1,4 +1,4 @@
-console.log("--------Exercise-------");
+console.log("--------Exercise---1----");
 // Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
 
 // Examples:
@@ -8,15 +8,15 @@ console.log("--------Exercise-------");
 // isLEQZero(-4) ➞ true
 // isLEQZero(10) ➞ false
 function isLEQZero(num) {
-    return num <= 0 ? "true" : "false";
+    console.log(num <= 0) ;
 }
 
-console.log(isLEQZero(3));
-console.log(isLEQZero(0));
-console.log(isLEQZero(-4));
-console.log(isLEQZero(10));
+isLEQZero(3);
+isLEQZero(0);
+isLEQZero(-4);
+isLEQZero(10);
 
-console.log("--------Exercise-------");
+console.log("--------Exercise---2----");
 
 //  Dog Years. Create a function which calculates how old a dog is in dog years. The function should accept one argument that is the dog's age in human years.
 //PS:  calculate the dog's age in dog years based on the calculation of 1 human year = 7 dog years.
@@ -32,7 +32,7 @@ function dogage(age) {
 dogage(4);
 dogage(6);
 
-console.log("--------Exercise-------");
+console.log("--------Exercise---3----");
 
 //  A Lifetime Supply...
 // You just won a lifetime supply of your favorite snack! The snack company needs to calculate what that actually means and how much they need to give you over your "lifetime". Create a function to help you calculate that amount for yourself to make sure you get the right amount. The function should accept two arguments: person age and the amount he will eats per day. The function should calculate the amount that will be consumed for the rest of your life given a constant maximum age of 100.
@@ -43,12 +43,18 @@ console.log("--------Exercise-------");
 
 // calcLifetimeSupply(40, 3) ➞ "The snack company should provide you with 65,745 units, until you are a ripe old age of 100. Happy snacking!"
 
-for (let i = 1 ; i <= 100; i++) {
-    ;
+const calcLifetimeSupply = (age, amount) => {
+    const maxAge = 100;
+    const yearsday = 365;
+    const result = (maxAge - age) * yearsday * amount;
+    console.log(
+      `The snack company should provide you with ${result} of Junk food, until you Die. :) Happy snacking!`);
+    }
 
-}
+calcLifetimeSupply(25, 2);
+calcLifetimeSupply(40, 3);
 
-console.log("--------Exercise-------");
+console.log("--------Exercise----4---");
 //  Number to Month Name
 // Create a function that takes a number (from 1 to 12) and returns its corresponding month name as a string.
 // For example, if you're given 3 as input, your function should return "March", because March is the 3rd month.
@@ -61,14 +67,15 @@ console.log("--------Exercise-------");
 // monthName(12) ➞ "December"
 // monthName(6) ➞ "June"
 
-let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
-function monthName(months){
-      console.log([months]);
+const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
+function monthName(months) {
+    console.log(months);
 }
-monthName(2);
-console.log(months[3])
 
-console.log("--------Exercise-------");
+monthName(2);
+console.log(months[3]);
+
+console.log("--------Exercise---5----");
 // Try to do it by yourself and don't look in yesterday solution. I trust you :)
 // Count Occurrences.
 // Create a function that accepts two arguments: a string and a letter. The function should count the number of occurrences of that letter in the string.
@@ -77,7 +84,7 @@ console.log("--------Exercise-------");
 
 // countOccurrences("this is a string", "i") ➞ 3
 
-console.log("--------Exercise-------");
+console.log("--------Exercise----6---");
 /*  draw this shapes in your console
 First shape
 T
@@ -99,7 +106,7 @@ Bounce: Forth shape
 Be creative and show us your Artistic side
 */
 
-console.log("--------Exercise-------");
+console.log("--------Exercise---7----");
 /*  Finish the following function so it outputs looks like this:
 1
 12
@@ -124,7 +131,7 @@ Bonus: create another function by doing some changes to the previous function in
   1
 */
 
-console.log("--------Exercise-------");
+console.log("--------Exercise----8---");
 /* create a function should that checks if a store contains a specific product in it's inventory.
 for example:
 let storeArr= ["milk", "eggs", "cheese", "butter"];
@@ -134,7 +141,7 @@ inventoryCheck("cheese") returns true
 inventoryCheck( "ham") returns false 
 */
 
-console.log("--------Exercise-------");
+console.log("--------Exercise----9---");
 // Extra work :)
 // create a repo for this app. we are coming later to it :)
 // Create a program that generate a random password with the length of 30 character, every time you call the function should give you a different password
