bdb483600fa61f99f196bccb6f55e59611ca434f
Omar <omar0940@gmail.com>
2021-01-13 15:07:01 +0100

    Assignment 13.1.21

--- jan/12-01/index.js
+++ jan/12-01/index.js
@@ -7,6 +7,14 @@ console.log("--------Exercise---1----");
 // isLEQZero(0) ➞ true
 // isLEQZero(-4) ➞ true
 // isLEQZero(10) ➞ false
+
+//const isLEQZero = (num) => {
+//   return num <= 0;
+// };
+// console.log(22 == 0);
+
+// console.log(isLEQZero(3)); // end of way one
+
 function isLEQZero(num) {
     console.log(num <= 0) ;
 }
@@ -24,6 +32,13 @@ console.log("--------Exercise---2----");
 // Example:
 
 // dogAge(4) ➞ "Your doggy is 28 years old in dog years!"
+
+// const dogAge = (dogAgeInHuman) => {
+//   let result = dogAgeInHuman * 7;
+//   return `Your doggy is ${result} years old in dog years!`;
+// };
+
+// console.log(dogAge(4));
 
 const dogAge = (age) => {
   console.log(`Your doggy is ${age* 7} years old in dog years!`);
@@ -49,6 +64,16 @@ console.log("--------Exercise---3----");
 
 // calcLifetimeSupply(40, 3) ➞ "The snack company should provide you with 65,745 units, until you are a ripe old age of 100. Happy snacking!"
 
+// const calcLifetimeSupply = (age, amountPerDay) => {
+//   // let result = 0;
+//   let amountPerYear = amountPerDay * 365;
+//   let yearsToLive = 100 - age;
+//   let result = amountPerYear * yearsToLive;
+
+//   return `The snack company should provide you with ${result} units, until you are a ripe old age of 100. Happy snacking!`;
+// };
+// console.log(calcLifetimeSupply(40, 3));
+
 const calcLifetimeSupply = (age, amount) => {
     const yearsday = 365;
     const maxAge = 100;    
@@ -73,17 +98,44 @@ console.log("--------Exercise----4---");
 // monthName(12) ➞ "December"
 // monthName(6) ➞ "June"
 
-let months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
-function monthName(monthnum) {
-    console.log( months[monthnum - 1]);
-}
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-monthName(2);
-monthName(9);
-monthName(7);
-monthName(8);
-monthName(5);
-monthName(1);
+const monthName = (monthNum) => {
+  let theNum = monthNum - 1;
+  let result = "";
+  if (theNum >= 0 && theNum <= 11) {
+    result = months[theNum];
+  } else {
+    result = `Sorry, you need to enter number between 1 and 12`;
+  }
+  return result;
+};
+console.log(monthName(3000));
+console.log(monthName(1));
+
+// function monthName(monthnum) {
+//     console.log( months[monthnum - 1]);
+// }
+
+// monthName(2);
+// monthName(9);
+// monthName(7);
+// monthName(8);
+// monthName(5);
+// monthName(1);
 
 
 console.log("--------Exercise---5----");
@@ -95,8 +147,26 @@ console.log("--------Exercise---5----");
 
 // countOccurrences("this is a string", "i") ➞ 3
 
-const countOccurrences = (str, ltr) => str.split(ltr).length -1;
-console.log(countOccurrences('this is a string', 'i'));
+const countOccurrences = (str, letter) => {
+  //let result = str.split("");
+  //console.log(result);
+  // let counter = 0;
+  // for (let i = 0; i < result.length; i++) {
+  //   if (result[i] == letter) {
+  //     counter++;
+  //   }
+  // }
+  let result = str.split(letter).length - 1;
+
+  return result;
+};
+
+console.log(countOccurrences("this is a string", "i")); // 3
+console.log(countOccurrences("xoxoxoxoxoxox", "x")); //7
+console.log(countOccurrences("x", "Y")); // 0
+
+// const countOccurrences = (str, ltr) => str.split(ltr).length -1;
+// console.log(countOccurrences('this is a string', 'i'));
 
 console.log("--------Exercise----6---");
 /*  draw this shapes in your console
@@ -145,6 +215,29 @@ Bonus: create another function by doing some changes to the previous function in
   1
 */
 
+const numbersCount = () => {
+  let text;
+  //           4>=0
+  for (let i = 5; i >= 0; i--) {
+    // something
+    // i = 3
+    text = "";
+    // text = ""
+    //                5<=4
+    for (let j = 1; j <= i; j++) {
+      // j= 5
+      // something
+      text += j;
+      // "123"+ 4
+    }
+    // something
+    console.log(text);
+    // "1234"
+  }
+};
+
+numbersCount();
+
 console.log("--------Exercise----8---");
 /* create a function should that checks if a store contains a specific product in it's inventory.
 for example:
@@ -154,6 +247,14 @@ let storeArr= ["milk", "eggs", "cheese", "butter"];
 inventoryCheck("cheese") returns true
 inventoryCheck( "ham") returns false 
 */
+
+let storeArr = ["milk", "eggs", "cheese", "butter"];
+const inventoryCheck = (str) => {
+  return storeArr.includes(str);
+};
+
+console.log(inventoryCheck("ham"));
+console.log(inventoryCheck("cheese"));
 
 console.log("--------Exercise----9---");
 // Extra work :)
@@ -165,4 +266,183 @@ console.log("--------Exercise----9---");
 // passwordGen()  ➞ Y3mI9x7bvPY86T?oM§hIbgh!MLwA77N
 // passwordGen()  ➞ Oy$tkBGoo5fg§mO4AdV&Kh8XXZR4d7K
 // passwordGen()  ➞ v0ZwDrn@hfI€kOKWH§k6kqub6zfpuyP
-console.log("--------Exercise-------");
+
+
+const passwordGen = () => {
+  let characters = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    6,
+    7,
+    8,
+    9,
+    "!",
+    "§",
+    "$",
+    "%",
+    "&",
+    "?",
+    "@",
+    "€",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let result = "";
+  let randomNum;
+
+  for (let i = 0; i <= 30; i++) {
+    randomNum = Math.floor(Math.random() * characters.length);
+    // result = result + characters[randomNum];
+    result += characters[randomNum];
+  }
+  return result;
+};
+
+// console.log(passwordGen());
+// other way
+let numbers = [0, 1, 2, 3, 4, 5, 6, 6, 7, 8, 9];
+let alpha = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let charactersSp = ["!", "§", "$", "%", "&", "?", "@", "€"];
+const passGen = () => {
+  let result = "";
+  const randomNumGetter = (arr) => {
+    return Math.floor(Math.random() * arr.length);
+  };
+  for (let i = 0; i <= 17; i++) {
+    if (i % 2 == 0) {
+      result += numbers[randomNumGetter(numbers)];
+    }
+    if (i % 3 == 0) {
+      result += alpha[randomNumGetter(alpha)].toUpperCase();
+    } else {
+      result += alpha[randomNumGetter(alpha)];
+    }
+    if (i % 5 == 0) {
+      result += charactersSp[randomNumGetter(charactersSp)];
+    }
+  }
+  return result;
+};
+
+console.log(passGen());
+console.log("--------Exercise----13.1.2021---");
+
+// XO Create a function that takes a string, checks if it has the same number of 'x's and 'o's and returns either true or false.
+
+// Notes:
+
+// Return a boolean value (true or false).
+// The string can contain any character.
+// When neither an x nor an o is in the string, return true.
+// Examples:
+
+// isEqualNumXandO("ooxx") ➞ true
+// isEqualNumXandO("xooxx") ➞ false
+// isEqualNumXandO("ooxXm") ➞ true (case insensitive)
+// isEqualNumXandO("zpzpzpp") ➞ true (returns true if no x and o)
+// isEqualNumXandO("zzoo") ➞ false
+const isEqualNumXandO = (str) => {
+  let newStr = str.toLowerCase();
+  let counterX = 0;
+  let counterO = 0;
+  for (let i = 0; i < newStr.length; i++) {
+    if (newStr[i] == "x") {
+      counterX++;
+    }
+    // if (newStr[i] == "o") {
+    //   counterO++;
+    // }
+    if (newStr.includes("o")) {
+      counterO++;
+    }
+  }
+
+  if (counterX === counterO) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isEqualNumXandO("zpzpzpp"));
+console.log(isEqualNumXandO("zzOo"));
+console.log(isEqualNumXandO("xxOo"));
