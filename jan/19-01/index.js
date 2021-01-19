5c5a0734415ead90314447a58e4c8fb48057f435
Omar <omar0940@gmail.com>
2021-01-19 14:44:09 +0100

    Arrey 19.1.21

--- jan/19-01/index.js
+++ jan/19-01/index.js
@@ -128,3 +128,45 @@ const findGreatest = (arr, num) => {
 };
 
 console.log(findGreatest([3, 4, 12, 44], 3));
+
+// AEIOU:
+// Vowels. Create a function that takes a string in its parameters and counts the number of vowels (i.e. in English, “a, e, i, o, u”) in the string.
+// i.e. findVowels(“this is a string”) ➞ 4
+const findVowels = (str) => {
+  let Vowels = [];
+  for (let i = 0; i < Vowels.length; i++) {
+    if ((str = findVowels.includes("a, e, i, o, u")));
+    console.log(findVowels);
+  }
+
+  console.log(4);
+};
+findVowels("this is a string");
+
+/* No Duplicates!
+  A set is a collection of unique items. A set can be formed from an array by removing all duplicate items. Create a program which transforms an array into a set of unique values. See the examples below. Example:
+ [1, 4, 4, 7, 7, 7] ➞ [1, 4, 7]
+ [1, 6, 6, 9, 9] ➞ [1, 6, 9]
+ [2, 2, 2, 2, 2, 2] ➞ [2]
+ [5, 10, 15, 20, 25] ➞ [5, 10, 15, 20, 25] */
+
+//  const duplicate = (str) => {
+//     let str =
+//     return str.filter              }
+//  }
+
+/*Dictionary. (take your time on this pls ) also as tip you can use .includes()
+ Create a function that takes an initial string and an array of words, and returns a filtered array of the words that start with the same letters as the initial string.
+Notes:
+If none of the words match, return an empty array.
+Keep the filtered array in the same relative order as the original array of words.
+Examples:
+dictionary(“bu”, [“button”, “breakfast”, “border”]) ➞ [“button”]
+dictionary(“tri”, [“triplet”, “tries”, “trip”, “piano”, “tree”]) ➞ [“triplet”, “tries”, trip”]
+dictionary(“beau”, [“pastry”, “delicious”, “name”, “boring”]) ➞ [] */
+
+const dictionary = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str) [i].includes;
+  }
+};
