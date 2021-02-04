01a97b0fbd304cf6c7289f1b57fe6d15a1bebf9a
Omar <omar0940@gmail.com>
2021-02-04 15:53:22 +0100

    Assignment Objects 4.2.2021

--- feb/01-02/04-02/index.js
+++ feb/01-02/04-02/index.js
@@ -43,4 +43,17 @@ function updateRecords(object, id, prop, value) {
   return object;
 }
 
+// another way
+/* function updateRecords(object, id, prop, value) {
+  if (value === "") delete object[id][prop];
+  else if (prop === "tracks") {
+    object[id][prop] = object[id][prop] || []; // this is called shortcircuit evaluation, see below for explanation
+    object[id][prop].push(value);
+  } else {
+    object[id][prop] = value;
+  }
+
+  return object;
+} */
+
 console.log(updateRecords(collection, 5439, "artist", "ABBA"));
