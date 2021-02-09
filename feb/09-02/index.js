3a644b98cadb61268a772553dd5b88398b1a9149
Omar <omar0940@gmail.com>
2021-02-09 15:42:12 +0100

    Clones vs. References

--- feb/09-02/index.js
+++ feb/09-02/index.js
@@ -233,9 +233,24 @@ objectToArray({
   turtles: 4
 }) 
 Expected output:
-[["cats", 1], ["dogs", 2], ["turtles", 4]] 
-14:22 Uhr
-List Properties. Create a function that returns an array of properties of a javascript object.
+[["cats", 1], ["dogs", 2], ["turtles", 4]]  */
+
+const objectToArray = (obj) => Object.entries(obj);
+console.log(
+  objectToArray({
+    A: 1,
+    B: 2,
+    C: 3,
+  })
+);
+console.log(
+  objectToArray({
+    cats: 1,
+    dogs: 2,
+    turtles: 4,
+  })
+);
+/*List Properties. Create a function that returns an array of properties of a javascript object.
 Example
 let student = {
   name: "Mike", 
@@ -243,10 +258,23 @@ let student = {
   course: "English"
 }
 Expected output:
-["name", "class", "course"]
-Merge. Create a function that takes two objects as its parameters and merges them together into a new object.
+["name", "class", "course"]*/
+
+let student = {
+  name: "Mike",
+  class: "4A",
+  course: "English",
+};
+const objKeys = (student) => Object.keys(student);
+console.log(objKeys(student));
+
+/*Merge. Create a function that takes two objects as its parameters and merges them together into a new object.
 Example
 let first = {firstName: "John"}
 let last = {lastName: "Smith"}
 Expected output:
 {firstName: "John", lastName: "Smith"} */
+let first = { firstName: "John" };
+let last = { lastName: "Smith" };
+const mergeObj = Object.assign(first, last);
+console.log(mergeObj);
