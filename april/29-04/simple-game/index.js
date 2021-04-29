6247cc2ae7c6da256318ed50f82bf702d36bb9ae
Omar <omar0940@gmail.com>
2021-04-29 13:37:55 +0200

    simple Game

--- april/29-04/simple-game/index.js
+++ april/29-04/simple-game/index.js
@@ -1,20 +1,38 @@
 // What is Q key code? so here all the keys code you need to know
 // https://keycode.info/
 let hStandPoint = 0;
-let vStandPont = 0;
+let vStandPoint = 0;
 function goUp() {
-  hStandPoint -= 10;
-  mini.style.top = hStandPoint + "px";
+  vStandPoint -= 10;
+  mini.style.top = vStandPoint + "px";
 }
 function goDown() {
-  hStandPoint += 10;
-  mini.style.top = hStandPoint + "px";
+  vStandPoint += 10;
+  mini.style.top = vStandPoint + "px";
 }
 function goRight() {
-  vStandPoint += 10;
-  mini.style.left = vStandPoint + "px";
+  hStandPoint += 10;
+  mini.style.left = hStandPoint + "px";
 }
 function goLeft() {
-  vStandPoint -= 10;
-  mini.style.left = vStandPoint + "px";
+  hStandPoint -= 10;
+  mini.style.left = hStandPoint + "px";
 }
+
+function move(e) {
+  console.log(e);
+  if (e.code == "KeyW") goUp();
+  if (e.code == "KeyD") goRight();
+  if (e.code == "KeyA") goLeft();
+  if (e.code == "KeyY") goDown();
+  console.log(hStandPoint);
+  console.log(vStandPoint);
+  if (
+    window.innerHeight / 4 <= vStandPoint &&
+    window.innerWidth - 70 <= hStandPoint
+  )
+    alert("cool you are winner");
+}
+const mini = document.querySelector(".mini");
+const body = document.querySelector("body");
+body.addEventListener("keypress", move);
