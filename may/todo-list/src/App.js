5454b004e1a44a2b805ee172bd91ff3000017ebd
Omar <omar0940@gmail.com>
2021-05-27 09:36:54 +0200

    ToDo list

--- may/todo-list/src/App.js
+++ may/todo-list/src/App.js
@@ -1,29 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [userInput, setUserInput] = useState("");
   const [list, setList] = useState([]);
-
-  function changeHandel(e) {
-    console.log(e.target.value);
-    setUserInput(e.target.value.trim());
-  }
-  function add(e) {
-    e.preventDefault();
-  }
-  return (
-    <div>
-      <form onSubmit={add}>
-        <input
-          type="text"
-          value={userInput}
-          onChange={changeHandel}
-          placeholder="type something"
-        />
-        <input type="submit" value="Add" />
-      </form>
-    </div>
-  );
+  return <React.Fragment></React.Fragment>;
 }
 
 export default App;
