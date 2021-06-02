7302bf2b36bbf675e1b99c9544eba743a364d212
Omar <omar0940@gmail.com>
2021-06-02 10:36:18 +0200

    clone instram with React

--- jun/02-06/src/App.js
+++ jun/02-06/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Gallery from "./components/Gallery";
+import Data from "./data.json";
 
 function App() {
+  // const [posts, setPosts] = useState(Data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {
+        // <Gallery posts={posts} />
+      }
+      <Gallery Data={Data} />
+    </React.Fragment>
   );
 }
 
