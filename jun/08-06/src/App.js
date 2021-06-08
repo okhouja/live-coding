99a346de2839b38bba3d1e11016865c2d71de9f7
Omar <omar0940@gmail.com>
2021-06-08 13:20:22 +0200

    Axios

---
+++ jun/08-06/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "./components/Loading";
+import Country from "./components/Country";
+const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  function changeHandle(e) {
+    setUserInput(e.target.value);
+  }
+  function submitHandle(e) {
+    e.preventDefault();
+    let textToUrl = encodeURIComponent(userInput);
+    console.log(textToUrl);
+    let endPoint = `https://restcountries.eu/rest/v2/name/${textToUrl}`;
+
+    // fetch(endPoint)
+    // .then((res)=> res.json())
+    // .then((data)=>setResults(data))
+
+    axios(endPoint)
+      .then(({ data }) => setResults(data))
+      .catch((err) => console.log(`Your had an ${err}`));
+  }
+  if (loading) return <Loading />;
+
+  return (
+    <React.Fragment>
+      <form onSubmit={submitHandle}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={changeHandle}
+          placeholder="Write a country name"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <Country results={results} />
+    </React.Fragment>
+  );
+};
+
+export default App;
