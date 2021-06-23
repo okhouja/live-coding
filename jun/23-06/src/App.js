b599c7eef73988183575da621a990747db44c73d
Omar <omar0940@gmail.com>
2021-06-23 13:07:21 +0200

    online-store

---
+++ jun/23-06/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Store, StoreContext } from "./context";
+import ProductList from "./components/Productlist";
+
+function App() {
+  return (
+    <StoreContext.Provider value={Store}>
+      <h1>Online Store</h1>
+
+      <ProductList />
+    </StoreContext.Provider>
+  );
+}
+
+export default App;
