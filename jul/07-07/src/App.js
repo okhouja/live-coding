962df47a2762f5061a578d7e239f7d5133a731e3
Omar <omar0940@gmail.com>
2021-07-13 10:44:15 +0200

    React life-cycle

--- jul/07-07/src/App.js
+++ jul/07-07/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import config from "./config.json";
+console.log(config.apiKey);
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+    };
+    this.text = "";
+  }
+  login = () => {
+    this.setState({ userName: "Max" });
+  };
+  // Never use
+  // componentWillMount() {
+  //   console.log("I run first :) ");
+  // }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ userName: "there" });
+    }, 2000);
+  }
+  componentDidUpdate() {
+    this.text = "I run after the update has been rendered in the Dom";
+  }
+  shouldComponentUpdate() {
+    return true;
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hey {this.state.userName}, I am React app</h1>
+        <h4>{this.text}</h4>
+        <button onClick={this.login}> Login</button>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
