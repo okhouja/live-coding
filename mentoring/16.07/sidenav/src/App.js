f167a6900986d30725a447251e15d7110bc75e8b
Omar <omar0940@gmail.com>
2021-07-16 14:17:27 +0200

    mentoring sidebar

---
+++ mentoring/16.07/sidenav/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./sass/main.scss";
+import "./components/Sidenav";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div>
+        <Sidenav />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ mentoring/16.07/sidenav/src/components/Sidenav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function sidenav() {
+  return <div></div>;
+}
+
+export default sidenav;
