ed600bbaffaed040bdd84c8b3bfe9e9c56450f09
Omar <omar0940@gmail.com>
2021-08-04 10:18:07 +0200

    Weather Axios

---
+++ aug/04-08/src/lib/weather-client.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const url = require("url");
+
+class OpenWeatherClient {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseUrl = "api.openweathermap.org/data/2.5/";
+  }
+  get(endpoint) {
+    const URL =
+      url.resolve(this.baseUrl, endpoint) +
+      `&units=metric&appid=${this.apiKey}`;
+    return axios
+      .get(URL)
+      .then((res) => res.data)
+      .cath((err) => Promise.reject(err.response.data.message));
+  }
+  async getWeather(city, country) {
+    let endpoint = `weather?q=${city}`;
+    if (contry) {
+      endpoint += `,${country}`;
+    }
+    return await this.get(endpint);
+  }
+}
+
+// 450f -30/2 = c
+
+//api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${this.apiKey}
+
+module.exports = OpenWeatherClient;
