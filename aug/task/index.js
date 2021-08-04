ba144c9867c59a91c2bf303addc63f7053a35f8a
Omar <omar0940@gmail.com>
2021-08-04 14:45:34 +0200

    task

---
+++ aug/task/index.js
@@ -0,0 +1,19 @@
+const { showHelp, showVersion } = require("./message");
+const args = process.argv.slice(2);
+
+if (args.includes("--help")) {
+  showHelp();
+}
+
+if (args.includes("-v")) {
+  showVersion();
+}
+
+prepareString = (str) => {
+  let newArray = str.map((word) => {
+    let fixedWord = word.trim();
+    return fixedWord.charAt(0).toUpperCase() + fixedWord.slice(1).toLowerCase();
+  });
+  console.log(newArray.join(" ").replace(/[^a-zA-Z ]/g, ""));
+};
+prepareString(args);
