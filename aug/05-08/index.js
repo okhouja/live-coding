6096478129394d085b543b4375779de1d4aa065e
Omar <omar0940@gmail.com>
2021-08-05 09:36:23 +0200

    create our server

--- aug/05-08/index.js
+++ aug/05-08/index.js
@@ -1,2 +1,14 @@
 require("dotenv").config();
-console.log(process.env.PORT);
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+// console.log(process.env.PORT);
+
+const firstRes = function (req, res) {
+  res.writeHead(200);
+  res.end("Hey, I am Server :)");
+};
+
+const server = http.createServer(firstRes);
+server.listen(PORT, () => console.log(`Server is listen on port=${PORT}`));
