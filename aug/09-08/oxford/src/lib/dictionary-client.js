d2a3ea33bbf847816d08ed462cff9f1e745e476e
Omar <omar0940@gmail.com>
2021-08-09 10:04:58 +0200

    oxford api

---
+++ aug/09-08/oxford/src/lib/dictionary-client.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const url = require("url");
+const config = require("config");
+
+class Dictionary {
+  constructor(apiID) {
+    this.apiID = "apiID";
+    this.apiKey = "apiKey";
+    this.baseUrl = "https://od-api.oxforddictionaries.com/api/v2";
+  }
+}
