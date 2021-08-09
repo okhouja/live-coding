3d360e49f9fad84e4ca4dc4b6ad9b62e4e2c7fa2
Omar <omar0940@gmail.com>
2021-08-09 14:06:36 +0200

    oxford api after working

--- aug/09-08/oxford/index.js
+++ aug/09-08/oxford/index.js
@@ -1,19 +1,75 @@
-// #!/usr/bin/env node
-// const dictionary = require("./lib/dictionary-client");
+// // #!/usr/bin/env node
+// // const dictionary = require("./lib/dictionary-client");
+// const axios = require("axios");
+// const { APP_ID, API_KEY } = require("./config");
+// const args = process.argv.slice(2);
+// const wordID = args;
+// const strictMatch = "false";
+
+// async function dictionary() {
+//   const myAPP_ID = APP_ID;
+//   const myApp_Key= API_KEY
+//   const headers = {
+//     Accept: "application/json",
+//     myAPP_ID: APP_ID,
+//     myApp_Key: API_KEY,
+//   };
+//   try {
+//     const { data } = await axios.get(
+//       `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${wordID}?fields=definitions&strictMatch=false
+//     `,
+//       { headers }
+//     );
+//     console.log(data);
+//   } catch (error) {
+//     console.log("Error:", error.response.data);
+//   }
+// }
+// dictionary();
+
+// // const options = {
+// //   host: "https://od-api.oxforddictionaries.com",
+// //   port: "443",
+// //   path: "/api/v2/entries/en-gb/" + wordID + "?strictMatch=" + strictMatch,
+// //   headers: {
+// //     Accept: "application/json",
+// //     app_id: APP_ID,
+// //     app_key: API_KEY,
+// //   },
+// // };
+
+// // const printDefinitions = async () => {
+// //   const definitions = await getDefinition();
+
+// //   if (definitions.data.results) {
+// //     console.log(
+// //       definitions.data.results[0].lexicalEntries[0].entries[0].senses[0]
+// //     );
+// //   }
+// // };
+
+// // const connection = axios.create({
+// //   baseURL: options.host,
+// //   headers: options.headers,
+// // });
+
+// // const getDefinition = async () => {
+// //   try {
+// //     return await connection.get(options.path);
+// //   } catch (error) {
+// //     console.error(error);
+// //   }
+// // };
+
+// // printDefinitions();
+
 const args = process.argv.slice(2);
 const wordID = args;
-
-require("dotenv").config();
-const http = require("http");
-
-const PORT = process.env.PORT || 5000;
 const axios = require("axios");
 const config = require("./config");
-
 async function dictionary() {
-  const myId = config.apiID;
-  const myKey = config.apiKey;
-
+  const myId = config.APP_ID;
+  const myKey = config.API_KEY;
   const headers = {
     Accept: "application/json",
     app_id: myId,
@@ -24,11 +80,11 @@ async function dictionary() {
       `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${wordID}?fields=definitions&strictMatch=false`,
       { headers }
     );
-
-    console.log(data.results.lexicalEntries);
+    console.log(
+      data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
+    );
   } catch (error) {
     console.log("Error:", error.response.data);
   }
 }
-
 dictionary(wordID);
