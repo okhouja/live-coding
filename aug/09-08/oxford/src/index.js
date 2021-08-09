697a8947032b7fa24a159c339304884b8342a782
Omar <omar0940@gmail.com>
2021-08-09 10:34:28 +0200

    oxford api

--- aug/09-08/oxford/src/index.js
+++ aug/09-08/oxford/src/index.js
@@ -1,3 +1,30 @@
-#!/usr/bin/env node
-const dictionary = require("./lib/dictionary-client");
-const args = process.argv.slice(2);
+// #!/usr/bin/env node
+// const dictionary = require("./lib/dictionary-client");
+// const args = process.argv.slice(2);
+
+require("dotenv").config();
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+const axios = require("axios");
+const config = require("../config");
+
+async function dictionary() {
+  const myId = config.apiID;
+  const myKey = config.apiKey;
+
+  const headers = {
+    app_id: myId,
+    app_key: myKey,
+  };
+  try {
+    const { data } = await axios.post(
+      "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/ace?strictMatch=false",
+      { headers }
+    );
+
+    console.log("Result data:", data);
+  } catch (error) {
+    console.log("Error:", error.response.data);
+  }
+}
--- aug/09-08/oxford/src/lib/dictionary-client.js
+++ aug/09-08/oxford/src/lib/dictionary-client.js
@@ -1,34 +1,22 @@
-const axios = require("axios");
-const url = require("url");
-const config = require("../../config");
+// const axios = require("axios");
+// const config = require("../../config");
 
-async function dictionary() {
-  const myId = config.apiID;
-  const myKey = config.apiKey;
+// async function dictionary() {
+//   const myId = config.apiID;
+//   const myKey = config.apiKey;
 
-  const headers = {
-    app_id: myId,
-    app_key: myKey,
-  };
-  try {
-    const { data } = await axios.post(
-      "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/ace?strictMatch=false",
-      { headers }
-    );
+//   const headers = {
+//     app_id: myId,
+//     app_key: myKey,
+//   };
+//   try {
+//     const { data } = await axios.post(
+//       "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/ace?strictMatch=false",
+//       { headers }
+//     );
 
-    console.log("Result data:", data);
-  } catch (error) {
-    console.log("Error:", error.response.data);
-  }
-}
-
-// class Dictionary {
-//   constructor(api) {
-//     this.apiID = "apiID";
-//     this.apiKey = "apiKey";
-//     this.baseUrl = "https://od-api.oxforddictionaries.com/api/v2";
-//   }
-//   get(endpoint){
-//       const URL= url.resolve(this.baseUrl,endpoint)+``
+//     console.log("Result data:", data);
+//   } catch (error) {
+//     console.log("Error:", error.response.data);
 //   }
 // }
