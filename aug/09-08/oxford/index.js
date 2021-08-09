71844754b32e025b186225ddd89e2ac60d784672
Omar <omar0940@gmail.com>
2021-08-09 20:10:19 +0200

    oxford api after working

--- aug/09-08/oxford/index.js
+++ aug/09-08/oxford/index.js
@@ -2,11 +2,10 @@ const args = process.argv.slice(2);
 const wordID = args;
 const axios = require("axios");
 const config = require("./config");
-async function dictionary() {
+const dictionary = async () => {
   const myId = config.APP_ID;
   const myKey = config.API_KEY;
   const headers = {
-    Accept: "application/json",
     app_id: myId,
     app_key: myKey,
   };
@@ -15,11 +14,19 @@ async function dictionary() {
       `https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/${wordID}?fields=definitions&strictMatch=false`,
       { headers }
     );
+    // console.log(data.results[0].lexicalEntries[0].entries[0].senses[0]);
     console.log(
-      data.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
+      data.results[0].lexicalEntries[0].entries[0].senses[0].subsenses
     );
   } catch (error) {
     console.log("Error:", error.response.data);
   }
-}
-dictionary(wordID);
+};
+
+const printDictionary = async () => {
+  const definitions = await dictionary();
+  if (definitions.data.results) {
+    console.log(`Got${data.results}`);
+  }
+};
+printDictionary(wordID);
