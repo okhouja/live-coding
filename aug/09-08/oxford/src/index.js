1ddacab80b688c1f78d33fc177bb0d03a3c9720e
Omar <omar0940@gmail.com>
2021-08-09 10:27:30 +0200

    oxford api

---
+++ aug/09-08/oxford/src/index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+const dictionary = require("./lib/dictionary-client");
+const args = process.argv.slice(2);
--- aug/09-08/oxford/src/lib/dictionary-client.js
+++ aug/09-08/oxford/src/lib/dictionary-client.js
@@ -1,11 +1,34 @@
 const axios = require("axios");
 const url = require("url");
-const config = require("config");
+const config = require("../../config");
 
-class Dictionary {
-  constructor(apiID) {
-    this.apiID = "apiID";
-    this.apiKey = "apiKey";
-    this.baseUrl = "https://od-api.oxforddictionaries.com/api/v2";
+async function dictionary() {
+  const myId = config.apiID;
+  const myKey = config.apiKey;
+
+  const headers = {
+    app_id: myId,
+    app_key: myKey,
+  };
+  try {
+    const { data } = await axios.post(
+      "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/ace?strictMatch=false",
+      { headers }
+    );
+
+    console.log("Result data:", data);
+  } catch (error) {
+    console.log("Error:", error.response.data);
   }
 }
+
+// class Dictionary {
+//   constructor(api) {
+//     this.apiID = "apiID";
+//     this.apiKey = "apiKey";
+//     this.baseUrl = "https://od-api.oxforddictionaries.com/api/v2";
+//   }
+//   get(endpoint){
+//       const URL= url.resolve(this.baseUrl,endpoint)+``
+//   }
+// }
