3b65d297e55bacc54ca7ae6f8945b2f255a7c2f5
Omar <omar0940@gmail.com>
2021-08-11 09:52:31 +0200

    GEt & POST

--- aug/11-08/index.js
+++ aug/11-08/index.js
@@ -3,6 +3,7 @@ require("dotenv").config;
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 // http://localhost:5000/
 app.get("/", (req, res) => {
@@ -22,6 +23,26 @@ app.get("/user", (req, res) => {
   console.log(req.query);
   const username = req.query.name;
   res.send(`<h2> Hey ${username}</h2>`);
+});
+// http://localhost:5000/login
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  /*
+  {
+	"userName":"Omar",
+	"pass":"000123fe345"
+}
+  */
+
+  const userName = req.body.userName;
+  const pass = req.body.pass;
+  if (userName === "Omar" && pass === "000123fe345") {
+    res.status(200).send(`Welcome Back <h1>${userName}</h1>`);
+  } else {
+    res.send(401).send("invalid username or pass");
+  }
+  res.send("cool");
 });
 
 const PORT = process.env.PORT || 5000;
