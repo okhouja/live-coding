4331f17d18bad94b8a9d87769005aae312eceae9
Omar <omar0940@gmail.com>
2021-08-11 09:26:52 +0200

    GEt & POST

--- aug/11-08/index.js
+++ aug/11-08/index.js
@@ -4,6 +4,18 @@ require("dotenv").config;
 const express = require("express");
 const app = express();
 
+// http://localhost:5000/
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("<h2> Welcome to our App</h2>");
+});
+
+// http://localhost:5000/about
+app.get("/about", (req, res) => {
+  console.log(req);
+  res.send("<h2> We are the coolest people</h2>");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
