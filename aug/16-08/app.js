f8ccc392446f215a79d1195514b732412b7c0584
Omar <omar0940@gmail.com>
2021-08-16 09:34:05 +0200

    Errors in Express

--- aug/16-08/app.js
+++ aug/16-08/app.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const app = express();
 
+app
+  .get("/", (req, res) => {
+    console.log("GET");
+    res.status(200).json({ text: "This is a GET request" });
+  })
+  .post("/", (req, res) => {
+    console.log("GET");
+    res.status(200).json({ text: "This is a POST request" });
+  })
+  .put("/", (req, res) => {
+    console.log("GET");
+    res.status(200).json({ text: "This is a PUT request" });
+  })
+  .delete("/", (req, res) => {
+    console.log("GET");
+    res.status(200).json({ text: "This is a DELETE request" });
+  });
+
 module.exports = app;
