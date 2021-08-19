19e877f3a78036e38a520a53171f8a365c7d375c
Omar <omar0940@gmail.com>
2021-08-19 09:09:28 +0200

    MongoDB

---
+++ aug/19-08/index.js
@@ -0,0 +1,14 @@
+// Structured Query Language aka SQL
+// ORACLE, MySQL
+// Most used SQl commends
+// SELECT, INSERT, DELETE, UPDATE
+
+// SELECT id,name,age FROM table
+// SELECT and FROM are SQL keywords
+
+// Not only SQL aka NoSQL
+// Humongous aka mongoDB
+// we have no tables but we call then collections in them we have documents(json like)
+// The name "BSON" is based on the term JSON and stands for "Binary JSON"
+
+// MongoDB today's commands
