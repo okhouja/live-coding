a9c993176cc96437347aa14d1a89c0ee6322e939
Omar <omar0940@gmail.com>
2021-08-30 10:33:43 +0200

    Fake Data

--- aug/30-08/app.js
+++ aug/30-08/app.js
@@ -18,13 +18,24 @@ mongoose
     console.log(`There was error ${err.message}`);
   });
 // console.log(path.resolve(__dirname, "views"));
-// console.log(faker.animal.cat());
+// console.log(faker.animal.dog());
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("home", { message: "Test" });
+  //   res.render("home", { message: "Test" });
+  fakeModel.find((err, data) => {
+    if (err) {
+      console.log(err);
+    } else if (data) {
+      // res.render("home", { data: data });
+      res.render("home", { data });
+    } else {
+      res.render("home", { data: {} });
+    }
+  });
 });
 app.post("/", (req, res) => {});
+
 module.exports = app;
