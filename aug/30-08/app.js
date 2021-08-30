93494ccb0e0c3f42dc5ec6caef9ebb29df314b23
Omar <omar0940@gmail.com>
2021-08-30 09:23:05 +0200

    Fake Data

---
+++ aug/30-08/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+const mongoose = require("mongoose");
+const faker = require("faker");
+// Get you the Path
+const path = require("path");
+//const fakeModel = require("./model/user");
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("DB is connected"))
+  .catch((err) => {
+    console.log(`There was error ${err.message}`);
+  });
+
+module.exports = app;
