6a8d7b8538a38c8e59aa2107c5ee5edf4533b438
Omar <omar0940@gmail.com>
2021-09-02 15:11:07 +0200

    Faker DATA new Version

--- aug/30-08/app.js
+++ aug/30-08/app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 app.use(morgan("dev"));
+// User controller
+const userController = require("./controller");
+
+// Get you the path
+const path = require("path");
 
 const mongoose = require("mongoose");
-const faker = require("faker");
-// Get you the
-const path = require("path");
-const fakeModel = require("./model/user");
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
@@ -17,40 +18,15 @@ mongoose
   .catch((err) => {
     console.log(`There was error ${err.message}`);
   });
-// console.log(path.resolve(__dirname, "views"));
-// console.log(faker.animal.dog());
 
+// App setting Engine
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
+//console.log(path.resolve(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  // res.render("home", { message: "Test" });
-  fakeModel.find((err, data) => {
-    if (err) {
-      console.log(err);
-    } else if (data) {
-      // res.render("home",{data:data})
-      res.render("home", { data });
-    } else {
-      res.render("home", { data: {} });
-    }
-  });
-});
-app.post("/", (req, res) => {
-  for (let i = 0; i < 10; i++) {
-    const fakeData = new fakeModel({
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      city: faker.address.city(),
-      imageUrl: faker.image.imageUrl(),
-    });
-    try {
-      fakeData.save();
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  res.redirect("/");
-});
+app
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(userController.addTenNewUsers);
 
 module.exports = app;
