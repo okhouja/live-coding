352e9d36fec3844048729dc86025248bb3004213
Omar <omar0940@gmail.com>
2021-09-02 15:11:34 +0200

    Faker DATA new Version

--- aug/30-08/server.js
+++ aug/30-08/server.js
@@ -11,9 +11,10 @@ To start new express project
  npm i faker
  npm i nodemon --save-dev
  touch server.js app.js
- mkdir model views
+ mkdir model views controller
  touch views/home.ejs
  touch model/user.js
+ touch controller/index.js
  json script
  "start": "nodemon server.js"
  // use users -> create new DB in mongo or use that if it's already there.
