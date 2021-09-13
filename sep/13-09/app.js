5c4a24457e12145293694f35c18087347da7f9e5
Omar <omar0940@gmail.com>
2021-09-13 09:44:21 +0200

    Relations part 2 population and ref

---
+++ sep/13-09/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+app.use(morgan("dev"));
+// Allow Express to understand json
+app.use(express.json());
+
+// Mongoose
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("DB is connected 😎"))
+  .catch((err) => {
+    console.log(`There was error ${err.message}`);
+  });
+
+module.exports = app;
