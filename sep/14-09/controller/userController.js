cf6c598a263e6bd18017eb8ed49d20d281f558b6
Omar <omar0940@gmail.com>
2021-09-14 15:17:55 +0200

    Authentications part one

--- sep/14-09/controller/userController.js
+++ sep/14-09/controller/userController.js
@@ -12,6 +12,7 @@ const userControllers = {};
 userControllers.getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
+    // console.log(users);
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({ message: err.message });
