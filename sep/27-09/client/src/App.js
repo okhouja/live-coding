eb43bf5d4f46463f4d71281123c20040e622a9cf
Omar <omar0940@gmail.com>
2021-09-27 11:06:38 +0200

    Full-stack App

--- sep/27-09/client/src/App.js
+++ sep/27-09/client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [users, setUsers] = useState([]);
+  const getData = () => {
+    fetch("/users")
+      // http://localhost:5000/users
+      .then((result) => result.json())
+      .then((data) => setUsers(data));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const usersLi = users.map((user, i) => <li key={i}>{user}</li>);
+  return <div className="App">{usersLi}</div>;
 }
 
 export default App;
